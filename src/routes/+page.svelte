95d357441e9ca4d46d32e2b970c7b2b94cdec991
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2024-02-10 22:49:29 +0100

    feat: first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const x = ""
+</script>
+
+<main class="flex h-screen">
+  <canvas class="fixed h-screen w-screen" />
+  <div class="m-auto text-center">
+    <b>ullrich</b>.tech 
+    <div class="mt-2 grid grid-cols-4 grid-rows-1">
+      <div class="px-4 border-r border-gray-500">xxx</div>
+      <div class="px-4 border-r border-gray-500">xxx</div>
+      <div class="px-4 border-r border-gray-500">xxx</div>
+      <div class="px-4">xxx</div>
+    </div>
+  </div>
+</main>
