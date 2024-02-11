d9d4c81591fc50ac2ef0462619526cc0a45212ac
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2024-02-11 15:01:17 +0100

    feat: add github workflow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,5 @@
   <canvas class="fixed h-screen w-screen" />
   <div class="m-auto text-center">
     <b>ullrich</b>.tech 
-    <div class="mt-2 grid grid-cols-4 grid-rows-1">
-      <div class="px-4 border-r border-gray-500">xxx</div>
-      <div class="px-4 border-r border-gray-500">xxx</div>
-      <div class="px-4 border-r border-gray-500">xxx</div>
-      <div class="px-4">xxx</div>
-    </div>
   </div>
 </main>
