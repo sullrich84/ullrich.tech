138b9e0a43acc967658c59a3a18a79c28de7c46c
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2024-02-14 18:12:34 +0100

    chore: add retro css effects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,10 @@
 
 <main class="flex h-screen">
   <canvas class="fixed h-screen w-screen" />
-  <div class="m-auto text-center">
+  <div class="m-auto text-center scanlines">
     <div>
       <b>ullrich</b>.tech
     </div>
-    <p class="text-xs italic">hello world</p>
+    <p class="text-xs">hello world</p>
   </div>
 </main>
