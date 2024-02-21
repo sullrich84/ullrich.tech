6fdc0671d86a1b35f66cf5ee0704d8f487e841e1
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2024-02-21 23:58:00 +0100

    chore: wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   <div class="flex flex-col justify-center items-center h-screen w-full animate-scanlines">
     <div class="text-center">
       <p><b>ullrich</b>.tech</p>
-      <p class="text-xs">hello world</p>
+      <p class="text-xs">&raquo;hello world&laquo;</p>
     </div>
   </div>
 </main>
